77527c6602d4a7e5c4f6b6de9a7e1ab039754d08
Max <mxmivlev@gmail.com>
2024-07-05 23:27:55 +0100

    Add working Children + Wedding Dance Classes page

---
+++ app/wedding-dance/page.jsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: 'Adult Classes'
+};
+
+export default async function Page() {
+
+    return (
+        <>
+            <h1>Adult Classes</h1>
+        </>
+    );
+}
+
