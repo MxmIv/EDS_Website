c391b65c035beb36302ef2991cf663d0f76da940
Max <mxmivlev@gmail.com>
2024-07-06 00:35:08 +0100

    Modifying footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,12 +2,18 @@ import Link from 'next/link';
 
 export function Footer() {
     return (
-        <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
+        <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16 text-center">
+            <p className="text-sm mb-4">
+            </p>
             <p className="text-sm">
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="underline transition decoration-dashed text-primary underline-offset-8 hover:opacity-80">
-                    Next.js on Netlify
-                </Link>
+                © 2024 Edinburgh Dance School. All rights reserved.
+            </p>
+            <p className="text-sm">
+                Telephone: +44 1234 567890
+            </p>
+            <p className="text-sm">
+                Address: 123 Dance Street, Edinburgh, EH1 2AB, United Kingdom
             </p>
         </footer>
     );
-};
+}
