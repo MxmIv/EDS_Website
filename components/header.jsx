ef9b7c0b83d80f7b72161ea7b4fbf76610fddbf8
Max <mxmivlev@gmail.com>
2024-07-06 22:12:21 +0100

    edit burger menu

--- components/header.jsx
+++ components/header.jsx
@@ -21,12 +21,12 @@ export function Header() {
     };
 
     return (
-        <nav className="flex flex-wrap items-center justify-between gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
+        <nav className="relative flex flex-wrap items-center justify-between gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
                 <Image src={edinburghDanceSchoolLogo} alt="Edinburgh Dance School Logo" className="w-56 h-auto" />
             </Link>
             <button
-                className="block lg:hidden p-2 rounded focus:outline-none focus:ring"
+                className="block lg:hidden p-2 rounded focus:outline-none focus:ring z-20"
                 onClick={toggleMenu}
             >
                 <svg
@@ -36,19 +36,17 @@ export function Header() {
                     viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                          d={menuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}></path>
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
                 </svg>
             </button>
-            <div
-                className={`flex-col lg:flex-row flex-grow ${menuOpen ? 'flex' : 'hidden'} lg:flex lg:items-center lg:gap-x-4`}>
+            <div className={`absolute top-full left-0 w-full bg-white text-black lg:relative lg:flex lg:bg-transparent lg:text-white lg:flex-row lg:items-center lg:gap-x-4 ${menuOpen ? 'block' : 'hidden'} lg:block`}>
                 {!!navItems?.length && (
-                    <ul className="flex flex-col lg:flex-row gap-x-4 gap-y-1 lg:gap-y-0">
+                    <ul className="flex flex-col lg:flex-row gap-x-4 gap-y-1 lg:gap-y-0 p-4 lg:p-0">
                         {navItems.map((item, index) => (
                             <li key={index}>
                                 <Link
                                     href={item.href}
-                                    className="inline-block px-1.5 py-1 transition hover:opacity-80 sm:px-3 sm:py-2"
+                                    className="block px-4 py-2 transition hover:bg-gray-200 lg:hover:bg-transparent lg:px-3 lg:py-2"
                                 >
                                     {item.linkText}
                                 </Link>
