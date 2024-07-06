9de24bf050919e096222e52d54a64435a5e19b45
Max <mxmivlev@gmail.com>
2024-07-06 21:51:50 +0100

    change colours

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <head>
             <link rel="icon" href="/favicon.svg" sizes="any" />
         </head>
-        <body className="antialiased text-white bg-grey">
+        <body className="antialiased text-black bg-white">
         <div className="flex flex-col min-h-screen px-6 sm:px-12">
             <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                 <Header />
