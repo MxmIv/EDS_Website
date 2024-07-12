ca2576b82be5eca1beee3b77f5527200783d59c1
Max <mxmivlev@gmail.com>
2024-07-12 21:23:00 +0100

    editing buttons

--- components/ImageButton.jsx
+++ components/ImageButton.jsx
@@ -6,10 +6,12 @@ import Image from 'next/image';
 export default function ImageButton({ href, src, alt, title }) {
     return (
         <Link href={href} legacyBehavior>
-            <a className="image-button flex flex-col items-center no-underline relative">
-                <Image src={src} alt={alt} width={300} height={200} className="w-full max-w-xs" />
-                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-full text-center">
-                    <h2 className="text-2xl font-bold text-white drop-shadow-md">{title}</h2>
+            <a className="image-button flex flex-col items-center no-underline relative group">
+                <div className="relative transition-transform duration-300 ease-in-out group-hover:scale-105">
+                    <Image src={src} alt={alt} width={300} height={200} className="w-full max-w-xs" />
+                    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-full text-center">
+                        <h2 className="text-2xl font-bold text-white drop-shadow-md">{title}</h2>
+                    </div>
                 </div>
             </a>
         </Link>
