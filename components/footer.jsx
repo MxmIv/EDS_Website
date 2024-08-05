ce4d166bc0ed27ce44f1f35dd9c6d9d1dfaf78c9
Max <mxmivlev@gmail.com>
2024-08-05 23:48:14 +0100

    fix footer + add icons

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,20 +1,47 @@
+'use client';
+
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 export function Footer() {
     return (
-        <footer className="bg-gray-100 text-black pt-8 pb-12 sm:pt-3 sm:pb-10 text-center mt-8">
-            <p className="text-sm mb-4">
-                <Link href="/privacy-policy" className="underline">Privacy Policy</Link>
-            </p>
-            <p className="text-sm">
-                © 2024 Edinburgh Dance School. All rights reserved.
-            </p>
-            <p className="text-sm">
-                Telephone: +44 1234 567890
-            </p>
-            <p className="text-sm">
-                Address: 123 Dance Street, Edinburgh, EH1 2AB, United Kingdom
-            </p>
+        <footer className="bg-gray-100 text-black py-8 sm:py-10">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col items-center text-center space-y-4">
+                    <p className="text-sm">
+                        <Link href="/privacy-policy" legacyBehavior>
+                            <a className="underline">Privacy Policy</a>
+                        </Link>
+                    </p>
+                    <p className="text-sm">
+                        © 2024 Edinburgh Dance School. All rights reserved.
+                    </p>
+                    <p className="text-sm">
+                        Telephone: <a href="tel:+447703483809" className="underline">+44 77034 83809</a>
+                    </p>
+                    <p className="text-sm">
+                        Address: 132 Captains Road, Edinburgh, EH17 8DU, United Kingdom
+                    </p>
+                    <div className="flex justify-center space-x-6 mt-4">
+                        <Link href="https://www.facebook.com/EdinburghDance/" passHref legacyBehavior>
+                            <a target="_blank" rel="noopener noreferrer" className="text-black hover:text-blue-500 transition duration-300">
+                                <FontAwesomeIcon icon={faFacebookF} size="2x" />
+                            </a>
+                        </Link>
+                        <Link href="https://wa.me/447703483809" passHref legacyBehavior>
+                            <a target="_blank" rel="noopener noreferrer" className="text-black hover:text-green-500 transition duration-300">
+                                <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+                            </a>
+                        </Link>
+                        <Link href="https://www.instagram.com/edinburghdanceschool/?hl=en-gb" passHref legacyBehavior>
+                            <a target="_blank" rel="noopener noreferrer" className="text-black hover:text-pink-500 transition duration-300">
+                                <FontAwesomeIcon icon={faInstagram} size="2x" />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
